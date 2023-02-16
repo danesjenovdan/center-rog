dbffc5b821bb28f260ecfd0550a846753182527b
patricijab <patricija@danesjenovdan.si>
2023-02-16 08:28:27 +0100

    setup wagtail project

---
+++ rog/rog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ig!t8bbw1tz&4wl2oqwbrw5b@p@(!y@l$ym46+#xt+q7k%$#3w"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
