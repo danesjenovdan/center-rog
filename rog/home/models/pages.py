665d2c7c58cc42e9437dbead08be7dfdf4e89d1d
patricija <patricija@danesjenovdan.si>
2023-02-28 14:07:42 +0100

    add other pages

--- rog/home/models/pages.py
+++ rog/home/models/pages.py
@@ -1,7 +1,40 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
+    pass
+
+
+class ObjectListPage(Page):
+    intro_text = models.TextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro_text'),
+    ]
+
+
+class ObjectProfilePage(Page):
+    description = models.TextField(blank=True)
+
+
+class StudioPage(ObjectProfilePage):
+    pass
+
+
+class ResidencePage(ObjectProfilePage):
+    pass
+
+
+class MarketStorePage(ObjectProfilePage):
+    pass
+
+
+class LibraryPage(ObjectProfilePage):
+    pass
+
+
+class ContentPage(Page):
     pass
