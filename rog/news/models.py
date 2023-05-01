91768f8c605fa42b509c674c37d447b3c0fd0a6f
patricijab <patricija@danesjenovdan.si>
2023-05-01 19:26:25 +0200

    add news page card

--- rog/news/models.py
+++ rog/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
@@ -8,8 +9,24 @@ from wagtail.images.blocks import ImageChooserBlock
 from home.models import BasePage
 
 
+class NewsCategory(models.Model):
+    category_name = models.TextField()
+    color_scheme = models.CharField(
+        max_length=20,
+        choices=settings.COLOR_SCHEMES,
+        default="light-gray",
+    )
+
+    panels = [
+        FieldPanel('category_name'),
+        FieldPanel('color_scheme'),
+    ]
+
+
 class NewsPage(BasePage):
     short_description = models.TextField(blank=True)
+    thumbnail = models.ForeignKey('wagtailimages.Image', null=True, on_delete=models.SET_NULL, related_name='+')
+    category = models.ForeignKey(NewsCategory, null=True, on_delete=models.SET_NULL)
     hero_image = models.ForeignKey('wagtailimages.Image', null=True, on_delete=models.SET_NULL, related_name='+')
     body = RichTextField(blank=True, null=True)
     gallery = StreamField([
@@ -18,9 +35,11 @@ class NewsPage(BasePage):
 
     content_panels = Page.content_panels + [
         FieldPanel('short_description'),
+        FieldPanel('thumbnail'),
+        FieldPanel('category'),
         FieldPanel('hero_image'),
         FieldPanel('body'),
-        FieldPanel('gallery'),
+        # FieldPanel('gallery'),
 
     ]
 
