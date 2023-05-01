b7d86a8ef2dbda29ee910a938c0e5eab79e95191
patricijab <patricija@danesjenovdan.si>
2023-05-01 20:02:55 +0200

    add event card and category

--- rog/events/models.py
+++ rog/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
@@ -6,15 +7,33 @@ from wagtail.admin.panels import FieldPanel
 from home.models import BasePage
 
 
+class EventCategory(models.Model):
+    name = models.TextField()
+    color_scheme = models.CharField(
+        max_length=20,
+        choices=settings.COLOR_SCHEMES,
+        default="light-gray",
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('color_scheme'),
+    ]
+
+
 class EventPage(BasePage):
     short_description = models.TextField(blank=True)
     long_description = models.TextField(blank=True)
+    thumbnail = models.ForeignKey('wagtailimages.Image', null=True, on_delete=models.SET_NULL, related_name='+')
+    category = models.ForeignKey(EventCategory, null=True, on_delete=models.SET_NULL)
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     content_panels = Page.content_panels + [
         FieldPanel('short_description'),
         FieldPanel('long_description'),
+        FieldPanel('thumbnail'),
+        FieldPanel('category'),
         FieldPanel('start_time'),
         FieldPanel('end_time'),
     ]
