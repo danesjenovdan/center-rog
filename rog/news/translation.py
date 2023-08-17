7b39df04d339a1d69a89ef0d78a2514cfc20df54
Tomaž kunst <cofek0@gmail.com>
2023-08-17 15:24:37 +0200

    added translations for all fields

---
+++ rog/news/translation.py
@@ -0,0 +1,20 @@
+from .models import NewsListPage, NewsListArchivePage, NewsPage, NewsListPage
+from modeltranslation.translator import TranslationOptions
+from modeltranslation.decorators import register
+
+@register(NewsListPage)
+class NewsListPageTR(TranslationOptions):
+    pass
+
+
+@register(NewsListArchivePage)
+class NewsListArchivePageTR(TranslationOptions):
+    pass
+
+
+@register(NewsPage)
+class NewsPageTR(TranslationOptions):
+    fields = (
+        'short_description',
+        'body'
+    )
