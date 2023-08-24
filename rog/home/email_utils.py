496495aed8560487f3188288676c4c5ad4b15a5a
Patricija Brečko <patricija@danesjenovdan.si>
2023-08-24 11:41:11 +0200

    add email utils

---
+++ rog/home/email_utils.py
@@ -0,0 +1,19 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+
+def send_email(to_email, template_name, subject, context):
+    html_body = render_to_string(template_name, context)
+    text_body = strip_tags(html_body)
+
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        from_email=settings.FROM_EMAIL,
+        to=[to_email],
+        body=text_body
+    )
+    msg.attach_alternative(html_body, "text/html")
+    msg.send()
+
