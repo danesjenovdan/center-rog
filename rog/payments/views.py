d06a8128a0b4e7a4771bee5cf9bb7d72e7ab0b68
Tomaž kunst <cofek0@gmail.com>
2023-11-20 16:10:03 +0100

    fixed promo code

--- rog/payments/views.py
+++ rog/payments/views.py
@@ -106,10 +106,10 @@ class PaymentPreview(views.APIView):
                         payment_plan.save()
                         plan = payment_plan.plan
                         plan_price = plan.discounted_price if user.is_eligible_to_discount() else plan.price
-                        payment_plan.price = plan_price
-                        payment_plan.save()
                         payment.amount -= plan_price * Decimal(valid_promo_code.percent_discount / 100)
                         payment.save()
+                        payment_plan.price = plan_price - plan_price * Decimal(valid_promo_code.percent_discount / 100)
+                        payment_plan.save()
                         promo_code_error = False
                         promo_code_success = True
 
