f9fae731a9aed3ace95d944cc69c487ba5c2c191
Patricija Brečko <patricija@danesjenovdan.si>
2023-11-29 10:34:02 +0100

    sort events to active/archived based on end date

--- rog/events/models.py
+++ rog/events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.utils.translation import gettext_lazy as _
