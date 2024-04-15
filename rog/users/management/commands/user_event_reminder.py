af0eab8f5c9f265f665104ab0e97976733a1aad9
Tomaž <cofek0@gmail.com>
2024-04-15 12:59:45 +0200

    fixed sending daily emails

--- rog/users/management/commands/user_event_reminder.py
+++ rog/users/management/commands/user_event_reminder.py
@@ -6,29 +6,40 @@ from payments.models import PaymentPlanEvent
 from events.models import EventPage
 from home.email_utils import send_email
 
+
 class Command(BaseCommand):
-    help = 'Reminder for membership expiration.'
+    help = "Reminder for membership expiration."
 
     def handle(self, *args, **options):
         self.stdout.write("Start sending notifications for user fee expiration.")
         events = EventPage.objects.filter(
-            start_day__range=(datetime.now().date(), (datetime.now() + timedelta(days=2)).date())
+            start_day__range=(
+                datetime.now().date(),
+                (datetime.now() + timedelta(days=2)).date(),
+            )
         )
         for event in events:
             for event_registration in event.event_registrations.all():
                 if event_registration.user:
-                    payment_plan_event = PaymentPlanEvent.objects.filter(event_registration=event_registration).first()
-                    if payment_plan_event and not payment_plan_event.notification_1_sent:
+                    payment_plan_event = PaymentPlanEvent.objects.filter(
+                        event_registration=event_registration
+                    ).first()
+                    if (
+                        payment_plan_event
+                        and not payment_plan_event.notification_1_sent
+                    ):
                         send_email(
                             event_registration.user.email,
-                            'emails/user_event_reminder.html',
-                            f'Center Rog – obvestilo prihajajočem dogodku: {event.title}',
+                            "emails/user_event_reminder.html",
+                            f"Center Rog – obvestilo prihajajočem dogodku: {event.title}",
                             {
                                 "event": event,
                             },
                         )
-                        payment_plan_event.notification_1_sent=True
+                        payment_plan_event.notification_1_sent = True
                         payment_plan_event.save()
+                        self.stdout.write(
+                            f"Email sent for event registration: {event_registration.id}"
+                        )
 
         self.stdout.write("End sending notifications for event.")
-
