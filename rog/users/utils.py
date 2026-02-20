f501587cb65a0305d542288f4ece82d746d83f7a
Tomaž Kunst <tomazkunst@gmail.com>
2026-02-20 13:50:57 +0100

    finish workshops selections

--- rog/users/utils.py
+++ rog/users/utils.py
@@ -50,3 +50,19 @@ def count_prima_users_without_names():
             i+=1
 
     print(users.count(), i)
+
+def flatten_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        elif isinstance(v, list):
+            for i, item in enumerate(v):
+                if isinstance(item, dict):
+                    items.extend(flatten_dict(item, f"{new_key}.{i}", sep=sep).items())
+                else:
+                    items.append((f"{new_key}.{i}", item))
+        else:
+            items.append((new_key, v))
+    return dict(items)
