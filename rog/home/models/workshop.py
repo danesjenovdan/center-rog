6ec5fdb0ffb28fe62121ab3273230fdd26a84dc2
Tomaž Kunst <tomazkunst@gmail.com>
2026-05-05 15:54:54 +0200

    added lab to workshops + v1 export users labs

--- rog/home/models/workshop.py
+++ rog/home/models/workshop.py
@@ -7,10 +7,12 @@ from wagtail.admin.panels import FieldPanel, InlinePanel
 class Workshop(models.Model):
     name = models.TextField(verbose_name=_("Ime usposabljanja"))
     prima_id = models.CharField(verbose_name=_("Prima ID"), max_length=255)
+    lab = models.ForeignKey("home.LabPage", verbose_name=_("Laboratorij"), on_delete=models.CASCADE, related_name="workshops", null=True, blank=True)
 
     panels = [
         FieldPanel("name"),
         FieldPanel("prima_id"),
+        FieldPanel("lab"),
     ]
 
     def __str__(self):
